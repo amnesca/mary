d02c03f54e16d28288cf26c15f33a8c18debd33e
Robson Tenório <rrtenorio@gmail.com>
2026-07-29 19:01:59 -0300

    Tabs: fix (#1110)
    * Tabs: fix
    
    * WIP
    
    * Update Tabs.php
    
    * Update Tab.php

--- src/View/Components/Tab.php
+++ src/View/Components/Tab.php
@@ -8,7 +8,7 @@ use Illuminate\View\Component;
 
 class Tab extends Component
 {
-    public string $uuid;
+
 
     public function __construct(
         public ?string $id = null,
@@ -20,36 +20,40 @@ class Tab extends Component
         public ?string $badge = null,
         public ?string $badgeClass = null,
     ) {
-        $this->uuid = "mary" . md5(serialize($this)) . $id;
+
     }
 
     public function render(): View|Closure|string
     {
         return <<<'HTML'
-                    @aware(['lift', 'box', 'labelClass', 'contentClass', 'activeClass'])
+                    @aware(['uuid', 'labelClass', 'contentClass', 'activeClass'])
 
                     <div>
-                        <label
-                            @class([
-                                "tab flex flex-nowrap items-center gap-3 whitespace-nowrap px-4",
-                                $labelClass,
-                                "hidden" => $hidden,
-                                "tab-disabled" => $disabled
-                             ])
-                            :class="{ '{{ $activeClass }}': selected === '{{ $name }}' }"
-                        >
-                            <input id="{{ ($id ?? $uuid).$name }}" type="radio" name="{{ ($id ?? $uuid).$name }}" value="{{ $name }}" x-model="selected" />
+                       <div wire:key="{{ $uuid() }}-tab-{{ $name }}">
+                            <template x-teleport="#{{ $uuid() }}-labels">
+                                <label
+                                    @class([
+                                        "tab flex flex-nowrap items-center gap-3 whitespace-nowrap px-4",
+                                        $labelClass,
+                                        "hidden" => $hidden,
+                                        "tab-disabled" => $disabled
+                                     ])
+                                    :class="{ 'tab-active {{ $activeClass }}': selected === '{{ $name }}' }"
+                                    @click="selected = '{{ $name }}'"
+                                >
 
-                            @if($icon)
-                              <x-mary-icon :name="$icon"  />
-                            @endif
+                                    @if($icon)
+                                      <x-mary-icon :name="$icon"  />
+                                    @endif
 
-                            {{ $label }}
+                                    {{ $label }}
 
-                            @if ($badge)
-                                <x-mary-badge :value="$badge" @class(["badge-sm badge-soft", $badgeClass]) />
-                            @endif
-                        </label>
+                                    @if ($badge)
+                                        <x-mary-badge :value="$badge" @class(["badge-sm badge-soft", $badgeClass]) />
+                                    @endif
+                                </label>
+                            </template>
+                        </div>
                         <div
                             x-show="selected == '{{ $name }}'"
                             {{ $attributes->class(["tab-content py-5 px-3 border-t-base-content/10 block rounded-none", $contentClass]) }}
--- src/View/Components/Tabs.php
+++ src/View/Components/Tabs.php
@@ -20,36 +20,23 @@ class Tabs extends Component
         $this->uuid = "mary" . md5(serialize($this)) . $id;
     }
 
+    public function uuid(): string
+    {
+        return $this->uuid.$this->attributes->wire('model')->value();
+    }
+
     public function render(): View|Closure|string
     {
         return <<<'HTML'
                     <div
-                        x-data="{
-                            selected: @entangle($attributes->wire('model')),
-                            init() {
-                                this.refresh();
-                                Livewire.hook('morphed', ({ el }) => this.refresh() );
-                            },
-                            refresh() {
-                                Array.from($refs.slot?.children ?? [])?.forEach(tab => {
-                                    const label = tab.querySelector('label');
-
-                                    if (label) {
-                                        $nextTick(() => {
-                                            $refs.labels.appendChild(label.cloneNode(true));
-                                            label.hidden = true
-                                        });
-                                    }
-                                });
-                            }
-                        }"
+                        x-data="{ selected: @entangle($attributes->wire('model')) }"
                         x-class="scrollbar-none flex-nowrap overflow-x-auto"
                     >
                         <!-- TABS -->
-                         <div x-ref="labels" {{ $attributes->except(['wire:model', 'wire:model.live'])->class(["tabs tabs-border", $tabsClass]) }}></div>
+                         <div id="{{ $uuid() }}-labels" wire:ignore {{ $attributes->except(['wire:model', 'wire:model.live'])->class(["tabs tabs-border", $tabsClass]) }}></div>
 
                         <!-- ORIGINAL DATA -->
-                         <div x-ref="slot">
+                         <div>
                             {{ $slot }}
                          </div>
                     </div>
